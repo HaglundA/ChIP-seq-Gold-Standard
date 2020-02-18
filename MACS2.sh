11058dd9a1e8666e037bfc86b4eff4b86241ecc6
Alexander Haglund <ah3918@ic.ac.uk>
2020-02-18 11:24:12 +0000

    Improved MACS2 loop

--- MACS2.sh
+++ MACS2.sh
@@ -1,12 +1,33 @@
 
 
 module load macs
-
 module load anaconda3/personal
 source activate Omega
 module load macs
 
 
 for i in /rds/general/user/ah3918/ephemeral/Janrefresh/chipseq/GSE*; do
-  macs2 callpeak -t $i/*duplicates.bam --outdir $i/MACS2output
+  for files in $i/*UNIQUE.bam; do
+    bname=`basename $files`
+    macs2 callpeak -t $files --outdir $i/MACS2output -n ${bname}
+  done
 done
+
+for i in /rds/general/user/ah3918/ephemeral/Janrefresh/chipseq/GSE*; do
+  for folders in $i/MACS2output; do
+      Rscript $folders/*UNIQUE.bam_model.r
+    done
+  done
+
+
+
+#Generates a PDF graph for every single .r model in all folders
+
+  for i in /rds/general/user/ah3918/ephemeral/Janrefresh/chipseq/GSE*; do
+    for folders in $i/MACS2output; do
+      for rmodels in $folders/*UNIQUE.bam_model.r; do
+        bname=`basename $rmodels`
+        Rscript ${bname}
+      done
+    done
+  done
